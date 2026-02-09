93198308eb007128c7ac52e50aa8e3b590c1a190
Nick Ustinov <nickustinov@gmail.com>
2026-02-09 10:00:49 +0000

    Add itsypad cask 1.0.1

--- Casks/itsypad.rb
+++ Casks/itsypad.rb
@@ -1,13 +1,18 @@
 cask "itsypad" do
-  version "1.0.0"
-  sha256 "569657b33113c7325c5259ccc9f43c10c6916ce1654a2c49211a9ea2edc529cb"
+  version "1.0.1"
+  sha256 "de8c47617301705a0cd2b433892697303108eaf237ac04bc7cccd420881dd2b1"
 
   url "https://github.com/nickustinov/itsypad-macos/releases/download/v#{version}/itsypad-#{version}.dmg"
   name "Itsypad"
-  desc "Tiny, native macOS text editor and clipboard manager"
+  desc "A tiny, fast scratchpad and clipboard manager for Mac"
   homepage "https://github.com/nickustinov/itsypad-macos"
 
   depends_on macos: ">= :sonoma"
 
   app "itsypad.app"
+
+  zap trash: [
+    "~/Library/Application Support/Itsypad",
+    "~/Library/Preferences/com.nickustinov.itsypad.plist",
+  ]
 end

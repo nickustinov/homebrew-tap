cb0dc4733862fdb8e49aab2f143339881e53dfa6
Nick Ustinov <nickustinov@gmail.com>
2026-02-09 13:05:17 +0000

    Update itsytv to 1.2.1

--- Casks/itsytv.rb
+++ Casks/itsytv.rb
@@ -1,6 +1,6 @@
 cask "itsytv" do
-  version "1.2.0"
-  sha256 "d784b085cd56b4f1c1127be65c05c744c08736d92221e47e88b6f6673ab2b9b5"
+  version "1.2.1"
+  sha256 "08b29390aec317be34828b5157c0d560c682d0da72c499b2b43b9987b2172636"
 
   url "https://github.com/nickustinov/itsytv-macos/releases/download/v#{version}/itsytv-#{version}.dmg"
   name "itsytv"

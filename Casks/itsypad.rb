ec5b89a5015fc1887c60837225d1dd76f97d79e8
Nick Ustinov <nickustinov@gmail.com>
2026-02-09 15:43:21 +0000

    Update itsypad to 1.0.5

--- Casks/itsypad.rb
+++ Casks/itsypad.rb
@@ -1,6 +1,6 @@
 cask "itsypad" do
-  version "1.0.4"
-  sha256 "e8d9f9437af84b671e923c77ff1bba5e4c7c5e2f47637c3198d0c814e9e3aa33"
+  version "1.0.5"
+  sha256 "e4a9f2083bb12f91d1ddece593e9512be92da2cbdb99b9ddce3d00987323fa41"
 
   url "https://github.com/nickustinov/itsypad-macos/releases/download/v#{version}/itsypad-#{version}.dmg"
   name "Itsypad"

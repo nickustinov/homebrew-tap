58fdb35e7f040533df71ae3d057c3b5fc986914f
Nick Ustinov <nickustinov@gmail.com>
2026-02-16 10:17:09 +0000

    Update itsypad to 1.8.0

--- Casks/itsypad.rb
+++ Casks/itsypad.rb
@@ -1,15 +1,15 @@
 cask "itsypad" do
-  version "1.6.0"
-  sha256 "47bd06baff2cbc8a8870da37591b95f9df34332cfddddfa58979d63362dbc0f9"
+  version "1.8.0"
+  sha256 "a3b1103646c688abaf23883b0a49424b744254b12d56d41750c775d155263c57"
 
-  url "https://github.com/nickustinov/itsypad-macos/releases/download/v#{version}/itsypad-#{version}.dmg"
+  url "https://github.com/nickustinov/itsypad-macos/releases/download/v#{version}/Itsypad-#{version}.dmg"
   name "Itsypad"
   desc "A tiny, fast scratchpad and clipboard manager for Mac"
   homepage "https://github.com/nickustinov/itsypad-macos"
 
   depends_on macos: ">= :sonoma"
 
-  app "itsypad.app"
+  app "Itsypad.app"
 
   zap trash: [
     "~/Library/Application Support/Itsypad",

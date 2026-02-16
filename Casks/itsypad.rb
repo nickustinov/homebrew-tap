c2ffac800c08b0184f4e653bb968695fe40486d0
Nick Ustinov <nickustinov@gmail.com>
2026-02-16 11:29:52 +0000

    Bump itsypad to 1.8.1

--- Casks/itsypad.rb
+++ Casks/itsypad.rb
@@ -1,6 +1,6 @@
 cask "itsypad" do
-  version "1.8.0"
-  sha256 "a3b1103646c688abaf23883b0a49424b744254b12d56d41750c775d155263c57"
+  version "1.8.1"
+  sha256 "e9107afa0a778b0f32d2b6f19a203bd532f37abf66978cba1fbd5d9d3bfc392f"
 
   url "https://github.com/nickustinov/itsypad-macos/releases/download/v#{version}/Itsypad-#{version}.dmg"
   name "Itsypad"

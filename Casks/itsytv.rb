28240f9e8f2e0216cd18da7c74b5ed50eba24f19
Nick Ustinov <nickustinov@gmail.com>
2026-02-17 21:06:22 +0000

    Update itsytv to 1.4.2

--- Casks/itsytv.rb
+++ Casks/itsytv.rb
@@ -1,13 +1,13 @@
 cask "itsytv" do
-  version "1.4.1"
-  sha256 "e554b388733f61f82e2627d292233cc810a98005d66228278d61cc62d29439cf"
+  version "1.4.2"
+  sha256 "b373f93e0791037baf8ec6e8f1230925197d6c135f42a6d90bd78695647cf964"
 
-  url "https://github.com/nickustinov/itsytv-macos/releases/download/v#{version}/itsytv-#{version}.dmg"
+  url "https://github.com/nickustinov/itsytv-macos/releases/download/v#{version}/Itsytv-#{version}.dmg"
   name "itsytv"
   desc "Control Apple TV from the macOS menu bar"
   homepage "https://github.com/nickustinov/itsytv-macos"
 
   depends_on macos: ">= :sonoma"
 
-  app "itsytv.app"
+  app "Itsytv.app"
 end
